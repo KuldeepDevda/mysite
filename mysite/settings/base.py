9620c40f00e8aa965294c8f35a1de56a5dd4b3fd
KuldeepDevda <kuldeep.thoughtwin@gmail.com>
2022-07-04 18:53:10 +0530

    make it better5

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -13,7 +13,6 @@ from pathlib import Path
 import os ,sys
 from decouple import config
 import django_heroku
-import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
